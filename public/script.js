3b92c5ccf123122c82f8b72515c39ac205d45916
Everton Lino <94932393+EvertonLDS815@users.noreply.github.com>
2024-06-05 18:45:02 -0300

    Update script.js on event.target

--- public/script.js
+++ public/script.js
@@ -9,16 +9,20 @@ controls.addEventListener("click", function (event) {
   let music = {};
 
   if (event.target.id != "controls") {
-    const musics =
-      event.path[2].childNodes[3].childNodes[5].childNodes[1].childNodes[3]
-        .childNodes;
+    let musics;
+    try {
+      musics = event.target.closest("#controls").querySelectorAll(".music-item");
+    } catch (error) {
+      console.error("Failed to get musics: ", error);
+      return;
+    }
 
     musics.forEach(function (item) {
       if (item.nodeName != "#text") {
-        music.name = item.childNodes[3].childNodes[0].data;
-        music.artist = item.childNodes[5].childNodes[0].data;
-        music.image = item.childNodes[1].childNodes[1].currentSrc;
-        music.audio = item.childNodes[7].childNodes[1];
+        music.name = item.querySelector(".music-name").innerText;
+        music.artist = item.querySelector(".music-artist").innerText;
+        music.image = item.querySelector(".music-image").src;
+        music.audio = item.querySelector(".music-audio");
         audios.push(music);
         music = {};
       }
@@ -34,10 +38,12 @@ controls.addEventListener("click", function (event) {
     document.querySelector("#volume").value = currentMusic.audio.volume * 100;
 
     const lichanged = document.querySelectorAll(".li-changed");
-    for(i = 0; i < lichanged.length; i++) {
-      lichanged[i].style.background = 'none'
+    for (let i = 0; i < lichanged.length; i++) {
+      lichanged[i].style.background = 'none';
     }
-    lichanged[index].style.background = '#1c1c1c';
+    if (lichanged[index]) {
+      lichanged[index].style.background = '#1c1c1c';
+    }
 
     const progressbar = document.querySelector("#progressbar");
     const textCurrentDuration = document.querySelector("#current-duration");
@@ -97,34 +103,34 @@ controls.addEventListener("click", function (event) {
   }
 
   if (event.target.id == "next-control") {
-      index++;
-      console.log(audios[index]);
-      if (index == audios.length) {
-        index = 0;
-      }
+    index++;
+    console.log(audios[index]);
+    if (index == audios.length) {
+      index = 0;
+    }
 
-      currentMusic.audio.pause();
-      updateDataMusic();
-      currentMusic.audio.play();
-      btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
+    currentMusic.audio.pause();
+    updateDataMusic();
+    currentMusic.audio.play();
+    btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
   }
 
   if (event.target.id == "prev-control") {
-      index--;
+    index--;
 
-      if (index == -1) {
-        index = audios.length -1;
-      }
+    if (index == -1) {
+      index = audios.length - 1;
+    }
 
-      currentMusic.audio.pause();
-      updateDataMusic();
-      currentMusic.audio.play();
-      btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
-      musicEnded();
+    currentMusic.audio.pause();
+    updateDataMusic();
+    currentMusic.audio.play();
+    btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
+    musicEnded();
   }
 
   function musicEnded() {
-    currentMusic.audio.addEventListener("ended", function() {
+    currentMusic.audio.addEventListener("ended", function () {
       index++;
 
       if (index == -1) {
@@ -135,7 +141,7 @@ controls.addEventListener("click", function (event) {
       updateDataMusic();
       currentMusic.audio.play();
       btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
-    })
+    });
   }
 });
 
